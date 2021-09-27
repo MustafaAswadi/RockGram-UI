4090aa75c336d61cef54c89e9e91801ef0a6097f
unknown <ab.etmadi@yahoo.com>
2021-09-27 13:58:32 +0430

    reactjs is created

---
+++ src/containers/home/Home.js
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div style={{textAlein: 'center'}}>
+            <h1>Welcome to RockGram!</h1>
+        </div>
+    )
+}
+
+
+
+export default Home
---
+++ src/globalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'style-components'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        list-style: border-box;
+    }
+
+    body{
+        background-color: slateblue;
+    }
+
+
+
+
+`;
+
+export default GlobalStyle
