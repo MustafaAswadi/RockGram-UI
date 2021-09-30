d9a8b88516b82460cad5eb185c7979b38e6c75a2
Mustafa Aswadi <aswadimustafa16@gmail.com>
2021-09-30 15:09:29 +0430

    signUp feature implemented

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,12 +1,38 @@
 import '../../assets/styles/login.css'
 import Logo from '../../assets/img/logo2.png'
 import main2 from '../../assets/img/main2.png'
-
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import Getapp from '../../shared/Getapp'
 import Header from '../../shared/Header'
+import Axios from 'axios'
+
+async function SignUpUser(credentials) {
+    await Axios({
+       method: 'post',
+       url: 'http://localhost:3001/user',
+       data: {fullname: credentials.fullName, email: credentials.email, password: credentials.password}
+   }).then(res => {
+       console.log(res.data);
+       return res.data;
+   })
+  }
 
 const Signup = () => {
+    const [email, setEmail] = useState();
+    const [fullName, setFullName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await SignUpUser({
+          email,
+          password,
+          fullName
+        });
+        console.log(token);
+
+      }
     return (
         <div className='container' style={{marginTop:'30px'}}>
             <div className='row align-items-center'>
@@ -16,19 +42,19 @@ const Signup = () => {
                    <Getapp/>
                 </div>
                 <div className="col">
-                    <form className='login-form cart'>
+                    <form className='login-form cart' onSubmit={handleSubmit}>
                         <div className='header'>
                         <img src={Logo} alt="rockgram logo"  className='login-logo'  />
                         <p className='header-p'><strong>Sign up to start a fascinating conversation with your friends.</strong></p>
                         </div>
                         <div className="mb-3">
-                            <input type="text" className="form-control" id="fullname" placeholder='Full Name'/>
+                            <input type="text" className="form-control" id="fullname" placeholder='Full Name' onChange={e => setFullName(e.target.value)}/>
                         </div>
                         <div className="mb-3">
-                            <input type="email" className="form-control" id="Email" placeholder='Email' aria-describedby="emailHelp"/>
+                            <input type="email" className="form-control" id="Email" placeholder='Email' aria-describedby="emailHelp" onChange={e => setEmail(e.target.value)}/>
                         </div>
                         <div className="mb-3">
-                            <input type="password" className="form-control" placeholder='Password' id="Password"/>
+                            <input type="password" className="form-control" placeholder='Password' id="Password" onChange={e => setPassword(e.target.value)}/>
                         </div>
                         <div className='d-grid gap-2'>
                             <button type="submit" className="btn btn-danger btn-login">Sign Up</button>
